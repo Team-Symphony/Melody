d775ff1dcef943b4eafbc4079b876cfb90d1b78d
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-09-27 18:30:29 +0100

    initial commit, mod setup directly from https://fabricmc.net/develop/template/

---
+++ src/client/java/dev/symphony/melody/MelodyClient.java
@@ -0,0 +1,10 @@
+package dev.symphony.melody;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MelodyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
