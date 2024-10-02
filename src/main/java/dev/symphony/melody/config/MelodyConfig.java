de73f76db3e5595d99829a52836eb6bbe49163b4
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-02 19:36:46 +0200

    Add MidnightLib for config.

---
+++ src/main/java/dev/symphony/melody/config/MelodyConfig.java
@@ -0,0 +1,7 @@
+package dev.symphony.melody.config;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class MelodyConfig extends MidnightConfig {
+    @Comment(category = "melody", centered = true) public static Comment reloadWarning;
+}
