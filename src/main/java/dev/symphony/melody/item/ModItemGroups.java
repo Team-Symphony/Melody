c9cf080ca81b4854e79706d59a49fac1ce919ba3
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-06 19:42:52 +0200

    Added translations for Netherite Horse Armor (also changed a translation id related to horse armor for consistency)

--- src/main/java/dev/symphony/melody/item/ModItemGroups.java
+++ src/main/java/dev/symphony/melody/item/ModItemGroups.java
@@ -12,7 +12,7 @@ import net.minecraft.util.Identifier;
 public class ModItemGroups {
     public static final ItemGroup MELODY_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(Melody.MOD_ID, "melody_item_group"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.NETHERITE_HORSE_ARMOR))
-                    .displayName(Text.translatable("melody.itemgroup.melody_item_group"))
+                    .displayName(Text.translatable("itemgroup.melody.melody_item_group"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.NETHERITE_HORSE_ARMOR);
 
