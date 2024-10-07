7062f6ad1b63ef779ab363736515537c1595ef63
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-07 20:12:32 +0200

    Made Horse Armor protect 15 instead of 11 (added custom netherite_horse_armor_material to make it possible)

--- src/main/java/dev/symphony/melody/item/ModItems.java
+++ src/main/java/dev/symphony/melody/item/ModItems.java
@@ -15,7 +15,8 @@ public class ModItems {
     // AUTHORS: Flatkat
     // ARTISTS: Eleanor
     public static final Item NETHERITE_HORSE_ARMOR = registerItem(
-            "netherite_horse_armor", new AnimalArmorItem(ArmorMaterials.NETHERITE, AnimalArmorItem.Type.EQUESTRIAN, false, new Item.Settings().maxCount(1).fireproof())
+            // Uses custom armor material in order to change the protection from 11 to 15 (since horse armor takes the chestplate protection from the material)
+            "netherite_horse_armor", new AnimalArmorItem(ModArmorMaterials.NETHERITE_HORSE_ARMOR_MATERIAL, AnimalArmorItem.Type.EQUESTRIAN, false, new Item.Settings().maxCount(1).fireproof())
     );
 
     public static Item registerItem(String name, Item item){
