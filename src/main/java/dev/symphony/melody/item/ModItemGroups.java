1ea7a3295f056dcc46dab3e2ea05550fdbdc3e60
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-12 23:02:07 +0200

    Changed creative tab icon from netherite horse armor to mapbook

--- src/main/java/dev/symphony/melody/item/ModItemGroups.java
+++ src/main/java/dev/symphony/melody/item/ModItemGroups.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
     public static final ItemGroup MELODY_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(Melody.MOD_ID, "melody_item_group"),
-            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.NETHERITE_HORSE_ARMOR))
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.MAP_BOOK))
                     .displayName(Text.translatable("itemgroup.melody.melody_item_group"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.NETHERITE_HORSE_ARMOR);
