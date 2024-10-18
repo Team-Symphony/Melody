dc62c6ac5131be9d29e26f6371074f2159e7742b
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-10-18 14:39:09 +0100

    only empty map books can now be put in the second cartography table slot

--- src/main/java/dev/symphony/melody/mixin/map_book/CartographyRecipesSlot1Mixin.java
+++ src/main/java/dev/symphony/melody/mixin/map_book/CartographyRecipesSlot1Mixin.java
@@ -3,6 +3,7 @@ package dev.symphony.melody.mixin.map_book;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.symphony.melody.config.MelodyConfig;
 import dev.symphony.melody.item.ModItems;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +13,6 @@ import org.spongepowered.asm.mixin.injection.At;
 public class CartographyRecipesSlot1Mixin {
     @ModifyReturnValue(at = @At("RETURN"), method = "canInsert")
     private boolean addCanInsert(boolean original, ItemStack stack) {
-        return original || (MelodyConfig.mapBook && (stack.isOf(Items.FILLED_MAP) || stack.isOf(ModItems.MAP_BOOK)));
+        return original || (MelodyConfig.mapBook && (stack.isOf(Items.FILLED_MAP) || (stack.isOf(ModItems.MAP_BOOK) && !stack.contains(DataComponentTypes.MAP_ID))));
     }
 }
