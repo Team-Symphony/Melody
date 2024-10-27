c4dda0b1694e8576e26b98f66b9d3c4cd3b5fe11
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-10-27 02:27:08 +0100

    Ported ModRecipes i think

--- src/main/java/dev/symphony/melody/item/ModRecipes.java
+++ src/main/java/dev/symphony/melody/item/ModRecipes.java
@@ -3,13 +3,13 @@ package dev.symphony.melody.item;
 import dev.symphony.melody.Melody;
 import dev.symphony.melody.item.map_book.MapBookAdditionRecipe;
 import dev.symphony.melody.item.map_book.MapBookCloningRecipe;
-import net.minecraft.recipe.SpecialRecipeSerializer;
+import net.minecraft.recipe.SpecialCraftingRecipe;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public final class ModRecipes {
-    public static final SpecialRecipeSerializer<MapBookAdditionRecipe> MAP_BOOK_ADDITION_SERIALIZER = new SpecialRecipeSerializer<>(MapBookAdditionRecipe::new);
-    public static final SpecialRecipeSerializer<MapBookCloningRecipe> MAP_BOOK_CLONING_SERIALIZER = new SpecialRecipeSerializer<>(MapBookCloningRecipe::new);
+    public static final SpecialCraftingRecipe.SpecialRecipeSerializer<MapBookAdditionRecipe> MAP_BOOK_ADDITION_SERIALIZER = new SpecialCraftingRecipe.SpecialRecipeSerializer<>(MapBookAdditionRecipe::new);
+    public static final SpecialCraftingRecipe.SpecialRecipeSerializer<MapBookCloningRecipe> MAP_BOOK_CLONING_SERIALIZER = new SpecialCraftingRecipe.SpecialRecipeSerializer<>(MapBookCloningRecipe::new);
 
     public static void registerRecipes() {
         Melody.LOGGER.info("Registering Recipes for: " + Melody.MOD_ID);
