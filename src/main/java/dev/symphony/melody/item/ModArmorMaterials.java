1d95a38da90437f057091caaf8b419515d5c54ce
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-11-01 20:18:09 +0100

    Fixed Netherite Horse Armor not giving protection (also removed unused imports and commented code)

--- src/main/java/dev/symphony/melody/item/ModArmorMaterials.java
+++ src/main/java/dev/symphony/melody/item/ModArmorMaterials.java
@@ -1,28 +1,19 @@
 package dev.symphony.melody.item;
 
-import dev.symphony.melody.Melody;
 import net.minecraft.item.equipment.ArmorMaterial;
 import net.minecraft.item.equipment.EquipmentModels;
 import net.minecraft.item.equipment.EquipmentType;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Util;
 
 import java.util.EnumMap;
-import java.util.function.Supplier;
 
 public class ModArmorMaterials {
 
     // For more info on this, compare to ArmorMaterials.NETHERITE on the vanilla sources
     public static final ArmorMaterial NETHERITE_HORSE_ARMOR_MATERIAL = new ArmorMaterial(37, Util.make(new EnumMap(EquipmentType.class), map -> {
-        map.put(EquipmentType.CHESTPLATE, 15);
+        map.put(EquipmentType.BODY, 15);
     // This j:15 is enchantability, while f:3 and g:0.1f are toughness and knockback resistance respectively
     }), 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, ItemTags.REPAIRS_NETHERITE_ARMOR, EquipmentModels.NETHERITE);
-
-    //public static RegistryEntry<ArmorMaterial> registerArmorMaterial(String name, Supplier<ArmorMaterial> material){
-    //    return Registry.registerReference(Registries.ARMOR_MATERIAL, Melody.id(name), material.get());
-    //}
 }
