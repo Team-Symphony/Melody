5392afc9a2e183a374e076cfe84299463e767eb9
Flatkat <87091676+Flatkat@users.noreply.github.com>
2024-11-06 19:03:13 +0100

    Added mixin that changes netherite horse armor protection

---
+++ src/main/java/dev/symphony/melody/mixin/transportation/ArmorMaterialsMixin.java
@@ -0,0 +1,17 @@
+package dev.symphony.melody.mixin.transportation;
+
+import net.minecraft.item.equipment.ArmorMaterials;
+import net.minecraft.item.equipment.EquipmentType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ArmorMaterials.class)
+public interface ArmorMaterialsMixin {
+    // Changes Netherite Horse Armor protection from vanilla value to a custom one
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void modifyNetheriteBodyProtection(CallbackInfo info) {
+        ArmorMaterials.NETHERITE.defense().put(EquipmentType.BODY, 15);
+    }
+}
